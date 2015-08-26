8ac78f2696b0260f95b52a648cb52e7b48192b66
Vikas Gorur <vikasgp@gmail.com>
2015-08-25 23:21:50 -0700

    Add config sample

---
+++ config.sample.py
@@ -0,0 +1,11 @@
+class Config:
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+
+    OAUTH_TOKEN = ''
+    OAUTH_TOKEN_SECRET = ''
+
+    EBOOKS_COMMAND = 'ebooks gen /Users/vikas/workspace/shakti_ebooks/model/shakti_shetty.model'
+
+    BOT_NAME = 'shakti_ebooks'
+    HANDLER_NAME = 'vikasgorur'
