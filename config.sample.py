ca786d14261dcb00e2b27ca9414c7a1cbb335b48
Vikas Gorur <vikasgp@gmail.com>
2015-08-29 01:10:23 -0700

    Update sample config

--- config.sample.py
+++ config.sample.py
@@ -1,11 +1,15 @@
 class Config:
+    # These are both for the app that belongs to the handler's account
     CONSUMER_KEY = ''
     CONSUMER_SECRET = ''
 
+    # Generate these tokens by running get_oauth_token.py
     OAUTH_TOKEN = ''
     OAUTH_TOKEN_SECRET = ''
 
-    EBOOKS_COMMAND = 'ebooks gen /Users/vikas/workspace/shakti_ebooks/model/shakti_shetty.model'
+    EBOOKS_COMMAND = 'ebooks gen /home/vikas/shakti_ebooks/model/shakti_shetty.model'
 
     BOT_NAME = 'shakti_ebooks'
     HANDLER_NAME = 'vikasgorur'
+
+    RUN_AT = [(11, 15), (0, 0)] # Times of day at which to send DMs (hh, mm)
