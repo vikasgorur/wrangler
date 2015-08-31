e3a942075f57e681e497ca5e64379c66011cc570
Vikas Gorur <vikasgp@gmail.com>
2015-08-30 18:24:08 -0700

    Use new conversation & peer objects every wakeup

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def next_run_time(conf):
     now = datetime.datetime.now()
     (now_h, now_m) = now.hour, now.minute
     times = sorted(conf.RUN_AT)
-    future_times = [(h, m) for (h, m) in times if h > now_h or h == now_h and m >= now_m]
-    print(future_times)
+    future_times = [(h, m) for (h, m) in times if h > now_h or h == now_h and m > now_m]
     if len(future_times) == 0:
         # nothing in the future today, so pick the first time tomorrow
         (h, m) = times[0]
@@ -34,10 +33,9 @@ def main():
                         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                         datefmt='%m-%d %H:%M')
     logger = logging.getLogger('main')
+    logger.info('starting up')
 
     ebooks = EbooksText(Config)
-    peer = TwitterPeer(Config)
-    conv = Conversation(ebooks, peer)
 
     if not validate_run_times(Config):
         logger.error('invalid run times specified')
@@ -45,6 +43,9 @@ def main():
 
     while True:
         ebooks.update()
+        peer = TwitterPeer(Config)
+        conv = Conversation(ebooks, peer)
+
         next_run = next_run_time(Config)
         logger.info('sleeping until {0}'.format(next_run))
         pause.until(next_run)
